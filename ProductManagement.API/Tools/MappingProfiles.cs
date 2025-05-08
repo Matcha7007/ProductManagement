843351e6518168338e876c7c627b2376ce5edd42
Matcha Latte <83999587+Matcha7007@users.noreply.github.com>
2025-05-09 01:12:10 +0700

    Add mapping profile

---
+++ ProductManagement.API/Tools/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+using ProductManagement.Common.Dtos.Product;
+using ProductManagement.Domain;
+
+
+namespace ProductManagement.API.Tools
+{
+	public class MappingProfiles : Profile
+	{
+		public MappingProfiles()
+		{
+			CreateMap<MstProduct, ProductDto>().ReverseMap();
+		}
+	}
+}
