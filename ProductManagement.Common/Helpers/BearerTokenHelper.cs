7287d9bd579a3b20aedfe27a8f507da3aef6d3f6
Matcha Latte <83999587+Matcha7007@users.noreply.github.com>
2025-05-09 01:07:47 +0700

    Add helpers

---
+++ ProductManagement.Common/Helpers/BearerTokenHelper.cs
@@ -0,0 +1,54 @@
+﻿using ProductManagement.Common.Extensions;
+
+using System.Security.Claims;
+
+namespace ProductManagement.Common.Helpers
+{
+	public static class BearerTokenHelper
+	{
+		public static string GenerateBearerToken(int userId)
+		{
+			List<Claim> listClaim = [];
+			listClaim.Add(new Claim("UserId", userId.ToString()));
+
+			return JWTTokenHelper.GenerateJWTToken(
+				"Yh2k7QSu4l8CZg5p6X3Pna9L0Miy4D3Bvt0JVr87UcOj69Kqw5R2Nmf4FWs03Hdx"
+				, "JWTAuthenticationSimpleEWallet"
+				, "JWTServiceSimpleEWalletClient"
+				, 5 > 0 ? DateTime.Now.AddMinutes(5) : null
+				, listClaim
+			);
+		}
+
+		public static bool ValidateBearerToken(string token)
+		{
+			return JWTTokenHelper.ValidateJWTToken(
+				"Yh2k7QSu4l8CZg5p6X3Pna9L0Miy4D3Bvt0JVr87UcOj69Kqw5R2Nmf4FWs03Hdx"
+				, "JWTAuthenticationSimpleEWallet"
+				, "JWTServiceSimpleEWalletClient"
+				, token);
+		}
+
+		public static int? GetUserId(string token)
+		{
+			string strUsedId = JWTTokenHelper.GetJWTClaimData(token, "UserId");
+			int? result = int.TryParse(strUsedId, out int parseResult)
+				? parseResult
+				: throw new FormatException("Error when getting user id from token.");
+			return result;
+		}
+
+		public static string GetTokenValue(string fullBearerToken)
+		{
+			string token = string.Empty;
+			if (fullBearerToken.IsNotNullOrWhiteSpace())
+			{
+				if (fullBearerToken.StartsWith("BEARER ", StringComparison.OrdinalIgnoreCase))
+				{
+					token = fullBearerToken.Substring(7);
+				}
+			}
+			return token;
+		}
+	}
+}
