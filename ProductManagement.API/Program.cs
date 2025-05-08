1631789cd7cf276ef04d539a3b582449ff15f5c9
Matcha Latte <83999587+Matcha7007@users.noreply.github.com>
2025-05-09 01:11:14 +0700

    Registered service

--- ProductManagement.API/Program.cs
+++ ProductManagement.API/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Persistence;
+
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+	.SetBasePath(Directory.GetCurrentDirectory())
+	.AddJsonFile("appsettings.json", optional: true)
+	.AddEnvironmentVariables();
 
 // Add services to the container.
 
@@ -6,6 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductManagementDbContext>(option =>
+{
+	string? serverDbConnectionString = builder.Configuration.GetConnectionString("DbProjectManagementConnection");
+	option.UseNpgsql(serverDbConnectionString);
+});
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
