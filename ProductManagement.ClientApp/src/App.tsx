02290119d2746266c36f2045a1e5d42e35b339bf
Matcha Latte <83999587+Matcha7007@users.noreply.github.com>
2025-05-09 08:20:38 +0700

    Add route and package

--- ProductManagement.ClientApp/src/App.tsx
+++ ProductManagement.ClientApp/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import ProductPage from "./pages/ProductPage";
+import RegisterPage from "./pages/RegisterPage";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [token, setToken] = useState<string | null>(() =>
+		localStorage.getItem("token")
+	);
+	const logoutTimerRef = useRef<NodeJS.Timeout | null>(null);
+
+	useEffect(() => {
+		if (token) {
+			localStorage.setItem("token", token);
+
+			if (logoutTimerRef.current) clearTimeout(logoutTimerRef.current);
+
+			logoutTimerRef.current = setTimeout(() => {
+				alert("Your session has expired. Please log in again.");
+				setToken(null);
+			}, 5 * 60 * 1000); // 5 menit
+		} else {
+			localStorage.removeItem("token");
+
+			if (logoutTimerRef.current) {
+				clearTimeout(logoutTimerRef.current);
+				logoutTimerRef.current = null;
+			}
+		}
+	}, [token]);
+
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<Navigate to={token ? "/products" : "/login"} />}
+			/>
+			<Route path="/login" element={<LoginPage setToken={setToken} />} />
+			<Route path="/register" element={<RegisterPage />} />
+			<Route
+				path="/products"
+				element={
+					token ? (
+						<ProductPage setToken={setToken} token={token} />
+					) : (
+						<Navigate to="/login" replace />
+					)
+				}
+			/>
+		</Routes>
+	);
 }
 
 export default App;
